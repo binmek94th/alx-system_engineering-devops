cf6b9a73c47530b2e2db85d7669ce1f467f54c22
Nehemiah Mekonnen <121231698+binmek94th@users.noreply.github.com>
2025-07-23 11:27:19 +0300

    Create 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,24 @@
+file { '/home/ubuntu/.ssh/config':
+  ensure  => present,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+}
+
+file_line { 'Declare identity file':
+  path  => '/home/ubuntu/.ssh/config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^(\s*)IdentityFile',
+}
+
+file_line { 'Turn off passwd auth':
+  path  => '/home/ubuntu/.ssh/config',
+  line  => '    PasswordAuthentication no',
+  match => '^(\s*)PasswordAuthentication',
+}
+
+file_line { 'Declare Host wildcard':
+  path  => '/home/ubuntu/.ssh/config',
+  line  => 'Host *',
+  match => '^Host \*',
+}
